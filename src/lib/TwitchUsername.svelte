5991d67a406d0c9793464563b4495a04906587d5
Samuel Meenzen <samuel@meenzen.net>
2023-12-19 23:05:44 +0100

    feat: show channel name in front of message

--- src/lib/TwitchUsername.svelte
+++ src/lib/TwitchUsername.svelte
@@ -14,8 +14,10 @@
     let spacer = $state(isAction ? actionSpacer : normalSpacer);
     let color = $state(c);
     let username = $state(message.tags.displayName);
+    let channel = $state(message.channel.substring(1));
 </script>
 
+<span class="channel-name message-text" style="color: white">[{channel}] </span>
 <span class="username message-text" style="color: {color}">{username}</span><span class="message-text">{spacer}</span>
 
 <style>
