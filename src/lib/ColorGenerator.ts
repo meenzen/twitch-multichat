d619a2e441402da773ea1cc2b8ab585db6680b84
Samuel Meenzen <samuel@meenzen.net>
2023-12-19 23:41:36 +0100

    feat: cache generated colors

--- src/lib/ColorGenerator.ts
+++ src/lib/ColorGenerator.ts
@@ -2,11 +2,20 @@ import chroma from "chroma-js";
 import randomizer from "seedrandom";
 
 const scale = chroma.scale("Spectral");
+const cache = new Map<string, string>();
 
 const ColorGenerator = {
     generate(seed: string) {
+        if (cache.has(seed)) {
+            return cache.get(seed);
+        }
+
         const random = randomizer(seed);
-        return scale(random()).hex();
+
+        const color = scale(random()).hex();
+        cache.set(seed, color);
+
+        return color;
     }
 }
 
