54d44a8d6db466c2f6828b93d20c2687756ead8d
Samuel Meenzen <samuel@meenzen.net>
2024-03-25 23:58:20 +0100

    chore: improved logos

--- vite.config.ts
+++ vite.config.ts
@@ -1,46 +1,57 @@
-import { sentryVitePlugin } from '@sentry/vite-plugin'
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vitest/config';
-import {SvelteKitPWA} from "@vite-pwa/sveltekit";
+import { sentryVitePlugin } from "@sentry/vite-plugin";
+import { sveltekit } from "@sveltejs/kit/vite";
+import { defineConfig } from "vitest/config";
+import { SvelteKitPWA } from "@vite-pwa/sveltekit";
 
 export default defineConfig({
-	build: {
-		sourcemap: true
-	},
-	plugins: [
-		sentryVitePlugin({
-			org: "meenzen",
-			project: "twitch-multichat",
-			url: "https://sentry.mnzn.dev",
-			authToken: process.env.SENTRY_AUTH_TOKEN,
-			telemetry: false,
-		}),
-		sveltekit(),
-		SvelteKitPWA({
-			registerType: "autoUpdate",
-			strategies: "injectManifest",
-			srcDir: "src",
-			filename: "service-worker.ts",
-			manifest: {
-				short_name: "Twitch Multichat",
-				name: "Twitch Multichat",
-				start_url: "/",
-				scope: "/",
-				display: "standalone",
-				theme_color: "#9146ff",
-			},
-			injectManifest: {
-				globPatterns: ["client/**/*.{js,css,ico,png,svg,webp,woff,woff2}"]
-			},
-			workbox: {
-				globPatterns: ["client/**/*.{js,css,ico,png,svg,webp,woff,woff2}"],
-			},
-			pwaAssets: {
-				image: "static/logo.svg",
-			},
-		})
-	],
-	test: {
-		include: ["src/**/*.{test,spec}.{js,ts}"]
-	}
+  build: {
+    sourcemap: true,
+  },
+  plugins: [
+    sentryVitePlugin({
+      org: "meenzen",
+      project: "twitch-multichat",
+      url: "https://sentry.mnzn.dev",
+      authToken: process.env.SENTRY_AUTH_TOKEN,
+      telemetry: false,
+    }),
+    sveltekit(),
+    SvelteKitPWA({
+      registerType: "autoUpdate",
+      strategies: "injectManifest",
+      srcDir: "src",
+      filename: "service-worker.ts",
+      manifest: {
+        short_name: "Twitch Multichat",
+        name: "Twitch Multichat",
+        start_url: "/",
+        scope: "/",
+        display: "standalone",
+        theme_color: "#9146ff",
+				lang: "en",
+        icons: [
+          {
+            src: "logo-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "logo-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+						purpose: "any maskable",
+          },
+        ],
+      },
+      injectManifest: {
+        globPatterns: ["client/**/*.{js,css,ico,png,svg,webp,woff,woff2}"],
+      },
+      workbox: {
+        globPatterns: ["client/**/*.{js,css,ico,png,svg,webp,woff,woff2}"],
+      },
+    }),
+  ],
+  test: {
+    include: ["src/**/*.{test,spec}.{js,ts}"],
+  },
 });
