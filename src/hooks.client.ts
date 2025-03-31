8850055d5cbe04977b5f557d0edee43c88a3dd29
Samuel Meenzen <samuel@meenzen.net>
2025-03-31 15:11:36 +0200

    feat: use official sentry sdk

--- src/hooks.client.ts
+++ src/hooks.client.ts
@@ -1,26 +1,20 @@
-import { init } from '@jill64/sentry-sveltekit-cloudflare/client'
-import * as Sentry from "@sentry/browser";
+import * as Sentry from "@sentry/sveltekit";
+import { dev } from "$app/environment";
 
-const onError = init(
-    'https://bf0e1a778faa45f43774793d79698924@o4508253646553088.ingest.de.sentry.io/4508253698064464',
-    {
-        sentryOptions: {
-            tunnel: "/api/tunnel",
-            integrations: [
-                Sentry.browserTracingIntegration(),
-                Sentry.browserProfilingIntegration(),
-                Sentry.replayIntegration(),
-            ],
-            tracesSampleRate: 1.0,
-            profilesSampleRate: 1.0,
-            // we only care about errors
-            replaysSessionSampleRate: 0,
-            replaysOnErrorSampleRate: 1.0,
-        },
-        enableInDevMode: false,
-    }
-)
+const onError = Sentry.init({
+  dsn: "https://bf0e1a778faa45f43774793d79698924@o4508253646553088.ingest.de.sentry.io/4508253698064464",
+  tunnel: "/api/tunnel",
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.browserProfilingIntegration(),
+    Sentry.replayIntegration(),
+  ],
+  tracesSampleRate: 1.0,
+  profilesSampleRate: 1.0,
+  // we only care about errors
+  replaysSessionSampleRate: 0,
+  replaysOnErrorSampleRate: 1.0,
+  enabled: !dev,
+});
 
-export const handleError = onError((e, sentryEventId) => {
-    // no special error handling
-})
+export const handleError = Sentry.handleErrorWithSentry();
--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,27 +1,22 @@
-// hooks.server.js
-import { init } from '@jill64/sentry-sveltekit-cloudflare/server'
-// or
-// import { serverInit } from '@jill64/sentry-sveltekit-cloudflare'
+import {
+  handleErrorWithSentry,
+  sentryHandle,
+  initCloudflareSentryHandle,
+} from "@sentry/sveltekit";
+import { sequence } from "@sveltejs/kit/hooks";
+import { dev } from "$app/environment";
 
-const { onHandle, onError } = init(
-    'https://bf0e1a778faa45f43774793d79698924@o4508253646553088.ingest.de.sentry.io/4508253698064464',
-    {
-        toucanOptions: {
-            tracesSampleRate: 1.0,
-        },
-        handleOptions: {
-            handleUnknownRoutes: false,
-        },
-        enableInDevMode: false,
-    }
-)
-
-export const handle = onHandle(async ({ event, resolve }) => {
+export const handle = sequence(
+  initCloudflareSentryHandle({
+    dsn: "https://bf0e1a778faa45f43774793d79698924@o4508253646553088.ingest.de.sentry.io/4508253698064464",
+    tracesSampleRate: 1.0,
+    enabled: !dev,
+  }),
+  sentryHandle(),
+  async ({ event, resolve }) => {
     const response = await resolve(event);
     response.headers.set("Document-Policy", "js-profiling");
     return response;
-})
-
-export const handleError = onError((e, sentryEventId) => {
-    // no special error handling
-})
+  }
+);
+export const handleError = handleErrorWithSentry();
